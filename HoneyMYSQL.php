63882db98fcaf6d39de5d3c6ff0beafb22a86f24
Bruno Garcia <41070501+brunograna@users.noreply.github.com>
2018-07-28 00:04:46 -0300

    Add files via upload

--- HoneyMYSQL.php
+++ HoneyMYSQL.php
@@ -174,32 +174,19 @@ class HoneyMYSQL{
     /**
      * This functions select the data from a MYSQL database with a condition
      * 
-     * Select_all - Should pass ($tablename, $attributes, $condition)  
-     * This functions select all the data from a MYSQL database
-     * 
-     * Select_all - Should pass ($tablename, $attributes, $values)  
+     * Select - Should pass ($tablename, $attributes, $condition)  
      * 
      * $tablename - Name of the table.
      * 
      * $attributes - Array with the attributes name.
      * 
-     * $condition - An String with one condition.      
-     *
-     * $values - Array with values of the attributes.   
+     * $condition - An String with a condition.    
      * 
-     * * Return array with the results in case of success
+     * * Return array with the results in case of success 
      * 
-     * 
-     * * Return 0 in case of not success
-     * 
-     * 
-     * * The values should be in the same sequence of the attributes name  
+     * * Return 0 in case of not success    
      */ 
-
-    public function select(String $tablename,array $attributes,String $condition ){        
-
-    public function select(String $tablename,array $attributes,String $clause ){        
-
+    public function select(String $tablename,array $attributes,String $condition ):array{        
         if ($this->connect->conectar()) {
             //Connected with database   
             //Here the SELECT query are made
@@ -212,10 +199,7 @@ class HoneyMYSQL{
                     $sql = $sql."`".$value."`, ";
                 }                
             }  
-
             $sql = $sql." FROM `".$tablename."` WHERE ".$condition;  
-
-            $sql = $sql." FROM `".$tablename."` WHERE ".$clause;  
                   
             //Verify with exist this data on database 
             $result = $this->connect->getConn()->query($sql);
@@ -228,7 +212,7 @@ class HoneyMYSQL{
                 }
                 return $array;
             }else{
-                echo '<br/><b>Not Found</b><br/>';
+                //echo '<br/><b>Not Found</b><br/>';
                 return array(0);                
             }
         }else{            
@@ -236,6 +220,100 @@ class HoneyMYSQL{
             
         }
     }
+
+    /**
+     * This functions update the data from a MYSQL database with a condition
+     * 
+     * Update - Should pass ($tablename, $attributes, $values, $condition)  
+     * 
+     * $tablename - Name of the table.
+     * 
+     * $attributes - Array with the attributes name.
+     * 
+     * $values - Array with the attributes values.
+     * 
+     * $condition - An String with a condition.    
+     * 
+     * * Return TRUE in case of success 
+     * 
+     * * Return FALSE in case of not success 
+     * 
+     * * The values should be in the same sequence of the attributes name  
+     */ 
+    public function update(String $tablename,array $attributes,array $values,String $condition ):bool{        
+        if ($this->connect->conectar()) {
+            //Connected with database   
+            //Here the UPDATE query are made
+            $sql = "UPDATE ".$tablename." SET ";          
+            //Mount the attributes with the values
+            foreach ($values as $key => $value) {                
+                if($key == count($attributes) - 1){
+                    $sql = $sql."`".$attributes[$key]."` = '".$value."'";
+                }else{
+                    $sql = $sql."`".$attributes[$key]."` = '".$value."', ";
+                }                
+            }  
+            //The condition is added
+            $sql = $sql." WHERE ".$condition;  
+            //echo $sql;      
+            //Execute the query
+            if ($this->connect->getConn()->query($sql) === TRUE) {
+                //Query was made with success                
+                $this->connect->fechar();
+                return true;
+            }else{
+                //Error in query
+                echo "<br/>Error in query : <b>". $this->connect->getConn()->error."</b><br/>";
+                $this->connect->fechar();
+                return false;
+            }             
+        }else{            
+            return false;
+            
+        }
+    }
+
+    /**
+     * This functions delete the data from a MYSQL database with a condition
+     * 
+     * Update - Should pass ($tablename, $attributes, $condition)  
+     * 
+     * $tablename - Name of the table.
+     * 
+     * $attributes - Array with the attributes name. 
+     * 
+     * $condition - An String with a condition.    
+     * 
+     * * Return TRUE in case of success 
+     * 
+     * * Return FALSE in case of not success  
+     */ 
+    public function delete(String $tablename,String $condition ):bool{        
+        if ($this->connect->conectar()) {
+            //Connected with database   
+            //Here the UPDATE query are made
+            $sql = "DELETE FROM ".$tablename."";             
+            //The condition is added
+            $sql = $sql." WHERE ".$condition;  
+            //echo $sql;      
+            //Execute the query
+            if ($this->connect->getConn()->query($sql) === TRUE) {
+                //Query was made with success                
+                $this->connect->fechar();
+                return true;
+            }else{
+                //Error in query
+                echo "<br/>Error in query : <b>". $this->connect->getConn()->error."</b><br/>";
+                $this->connect->fechar();
+                return false;
+            }             
+        }else{            
+            return false;
+            
+        }
+    } 
+
+
 
 
 
